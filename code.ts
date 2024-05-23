5540a26c0c40fb1f88e70836863de1afa3a898a5
Peter Loveland <peterloveland@github.com>
2024-05-23 12:12:19 +0100

    wrapping in helper functions

--- code.ts
+++ code.ts
@@ -1,38 +1,32 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
+// This file holds the main code for our plugin. This file is where we make  a call to the OpenAI API
+// and then do something with the response in the figma document.
+// We can have access to the figma document via the figma global object. (e.g. figma.currentPage.selection)
 
-// This file holds the main code for plugins. Code in this file has access to
-// the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
-
-// This shows the HTML page in "ui.html".
 figma.showUI(__html__);
 
+// ****************************************************************************************************************************************
+// THIS IS WHERE WE LISTEN FOR MESSAGES FROM THE UI AND THEN DO SOMETHING WITH THEM
 figma.ui.onmessage = async  (msg: {type: string, prompt: string}) => {
   
-  if (msg.type === 'ping') { // we listen for the message type 'hello-lgtm!'
-    figma.ui.postMessage({type: 'set_loading', isLoading: true });
-    figma.notify('Ping!'); // this is how you show a message in the UI
+  if (msg.type === 'ping') { // we listen for the message type 'ping!'
+    sendMessageToUI('set_loading', {isLoading: true}); // we use sendMessageToUI to send a message to the UI. We listen for these events in ui.html
+    notifyInFigma('Ping!'); // this is how you show an alert/toast inside the figma the UI
     setTimeout(() => {
-      figma.ui.postMessage({type: 'pong' });
-      figma.ui.postMessage({type: 'set_loading', isLoading: false });
+      sendMessageToUI('pong');
+      sendMessageToUI('set_loading', {isLoading: false});
     }, 2000);
 
   }
 
   if (msg.type === 'generate-ai') {
-    figma.ui.postMessage({type: 'set_loading', isLoading: true });
+    sendMessageToUI('set_loading', {isLoading: true });
     const OPENAI_API_KEY = ''; // replace with your actual API key
 
-    console.log("PROMPT IS", msg.prompt)
-
-    // const currentSelection = figma.currentPage.selection;
-
     const selectionCount = figma.currentPage.selection.length;
-    const allAppliedFilles = getAppliedColorOfNodes(figma.currentPage.selection);
+    const allAppliedFilles = getAppliedColorOfNodes();
 
     console.log("all applied fills", allAppliedFilles)
+    console.log({selectionCount})
 
     const response = await fetch('https://api.openai.com/v1/chat/completions', {
       method: 'POST',
@@ -47,7 +41,7 @@ figma.ui.onmessage = async  (msg: {type: string, prompt: string}) => {
         messages: [
           {
             role: 'system',
-            content: `You are a world class assistant to a user who needs you to help them. The user will give you a certain prompt and you will do as they say. You will never respond with anything other than the response JSON, an array of length ${selectionCount}. You will respond in an object that contains matches this schema: { result: 'string'[] }. Even if you are returning 0 or 1 result, always.`
+            content: `You are a world class assistant to a user who needs you to help them. The user will give you a certain prompt and you will do as they say. You will never respond with anything other than the response JSON, an array of length ${selectionCount}. You will respond in an object that contains matches this schema: { result: 'string'[] }. Even if you are returning 0 or 1 result, always. Never provide fewer results in the array than the ${selectionCount}`
           },
           { role: 'user', content: msg.prompt }
         ]
@@ -58,34 +52,96 @@ figma.ui.onmessage = async  (msg: {type: string, prompt: string}) => {
     console.log("Data is", data)
     if (data.error || !data.choices.length) {
       console.error(data || 'No response from OpenAI API')
+      notifyInFigma('Error: No response from OpenAI API');
     } else {
-      const response = data.choices[0].message.content;
-      const parsedResponse = JSON.parse(response);
-      await replaceMultipleNodesText(figma.currentPage.selection as TextNode[], parsedResponse.result);
+      const parsedResponse = parseOpenAIResponse(data);
+      DO_SOMETHING_WITH_RESPONSE(parsedResponse);
     }
   }
-  figma.ui.postMessage({type: 'set_loading', isLoading: false });
-};
-
-function getAppliedColorOfNodes(nodes: readonly SceneNode[]) {
-  return nodes.map(node => 'fills' in node && node.fills);
+  sendMessageToUI('set_loading', {isLoading: false });
 }
 
-function getAllTextCharactersOfNodes(nodes: TextNode[]) {
-  return nodes.map(node => node.characters);
+
+// ****************************************************************************************************************************************
+// THIS IS WHERE YOU CAN WRITE YOUR FUNCTION THAT WILL TAKE THE RESPONSE FROM OPENAI (WHICH IS AN ARRAY) AND DO SOMETHING WITH IT IN FIGMA
+// view the helper functions below to see how you can interact with the figma document
+
+const DO_SOMETHING_WITH_RESPONSE = (response:[]) => {
+  notifyInFigma(`Received ${response.length} responses from OpenAI: ${JSON.stringify(response)}`); // this is how you show a message in the UI
+  replaceMultipleNodesText(response);
+  return response;
 }
 
-async function replaceMultipleNodesText(nodes: TextNode[], textArray: string[]) {
-  //@ts-expect-error expecting
-  console.log("selection is", figma.currentPage.selection.map(node => node.characters))
-  console.log("Nodes are", nodes.map(node => node.characters))
-  nodes.forEach(async (node, index) => {
-    console.log("replacing", node.characters, "with", textArray[index])
-    await replaceTextOfNode(node, textArray[index]);
-  });
+
+
+
+
+//   _    _          _                            __                          _     _                       
+//  | |  | |        | |                          / _|                        | |   (_)                      
+//  | |__| |   ___  | |  _ __     ___   _ __    | |_   _   _   _ __     ___  | |_   _    ___    _ __    ___ 
+//  |  __  |  / _ \ | | | '_ \   / _ \ | '__|   |  _| | | | | | '_ \   / __| | __| | |  / _ \  | '_ \  / __|
+//  | |  | | |  __/ | | | |_) | |  __/ | |      | |   | |_| | | | | | | (__  | |_  | | | (_) | | | | | \__ \
+//  |_|  |_|  \___| |_| | .__/   \___| |_|      |_|    \__,_| |_| |_|  \___|  \__| |_|  \___/  |_| |_| |___/
+//                      | |                                                                                 
+//                      |_|                                                                                 
+
+// These are small functions we've written to help you interact with the figma document. You can use them in your code above.
+
+  // this is a snippet to get the color of the selected nodes
+  function getAppliedColorOfNodes() {
+    const nodes = figma.currentPage.selection as TextNode[]
+    return nodes.map(node => 'fills' in node && node.fills);
+  }
+
+  // this is a snippet to get the text of the selected nodes
+  function getAllTextCharactersOfNodes() {
+    const nodes = figma.currentPage.selection as TextNode[]
+    return nodes.map(node => node.characters);
+  }
+
+  // this is a snippet to replace the text of the selected nodes
+  async function replaceMultipleNodesText(textArray: string[]) {
+    const nodes = figma.currentPage.selection as TextNode[]
+    //@ts-expect-error expecting
+    console.log("selection is", figma.currentPage.selection.map(node => node.characters))
+    console.log("Nodes are", nodes.map(node => node.characters))
+    nodes.forEach(async (node, index) => {
+      console.log("replacing", node.characters, "with", textArray[index])
+      await replaceTextOfNode(node, textArray[index]);
+    });
+  }
+
+  // change variable of 
+
+  async function replaceTextOfNode(node: TextNode, newText: string) {
+    await figma.loadFontAsync(node.fontName as FontName);
+    node.characters = newText;
+  }
+
+
+
+// ****************************************************************************************************************************************
+//  __          __                 _            _                          __                          _     _                       
+//  \ \        / /                | |          | |                        / _|                        | |   (_)                      
+//   \ \  /\  / /    ___    _ __  | | __  ___  | |__     ___    _ __     | |_   _   _   _ __     ___  | |_   _    ___    _ __    ___ 
+//    \ \/  \/ /    / _ \  | '__| | |/ / / __| | '_ \   / _ \  | '_ \    |  _| | | | | | '_ \   / __| | __| | |  / _ \  | '_ \  / __|
+//     \  /\  /    | (_) | | |    |   <  \__ \ | | | | | (_) | | |_) |   | |   | |_| | | | | | | (__  | |_  | | | (_) | | | | | \__ \
+//      \/  \/      \___/  |_|    |_|\_\ |___/ |_| |_|  \___/  | .__/    |_|    \__,_| |_| |_|  \___|  \__| |_|  \___/  |_| |_| |___/
+//                                                             | |                                                                   
+//                                                             |_|                                                                   
+// These can mostly be ignored, they are just helper functions to make the code more readable and quick to write.
+// Feel free to use them or modify them as you see fit.
+
+const parseOpenAIResponse = (response) => {
+  const content = response.choices[0].message.content;
+  const parsedResponse = JSON.parse(content).result;
+  return parsedResponse;
 }
 
-async function replaceTextOfNode(node: TextNode, newText: string) {
-  await figma.loadFontAsync(node.fontName as FontName);
-  node.characters = newText;
+const sendMessageToUI = (type:string, message: any = {}) => {
+  figma.ui.postMessage({type: type, message});
+}
+
+const notifyInFigma = (message: string, timeout: number = 2000) => {
+  figma.notify(message, {timeout: timeout});
 }
