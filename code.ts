e70afed7fba61f66b0fd59d0f40c55a25a66625d
Peter Loveland <peterloveland@github.com>
2024-05-29 11:28:21 +0100

    revert to 4.0

--- code.ts
+++ code.ts
@@ -32,7 +32,7 @@ figma.ui.onmessage = async  (msg: {type: string, prompt: string}) => {
       },
       // https://platform.openai.com/docs/guides/text-generation/chat-completions-api
       body: JSON.stringify({
-        model: 'gpt-3.5-turbo',
+        model: 'gpt-3.5-turbo-1106',
         response_format: {'type':'json_object'},
         messages: [
           {
